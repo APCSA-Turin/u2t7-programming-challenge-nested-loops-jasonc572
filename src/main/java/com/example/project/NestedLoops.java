342564b96abafada38d47354418a43560987b96e
jasonc572 <jasonc572@nycstudents.net>
2025-12-10 18:39:46 +0000

    im smart

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -17,7 +17,16 @@ return the string below for given height=5
 *****  
            */
 public static String starStaircase(int height){
-    return "";
+    String answer = "";
+    for (int i = 1; i <= height; i++) {
+        for (int k = 1; k <= i; k++) {
+            answer = answer + "*";
+        }
+        if (i < height) {
+            answer = answer + "\n";
+        }
+    }
+    return answer;
 }
           /*
 return the string below for given height=5
@@ -29,7 +38,16 @@ return the string below for given height=5
            */
 
 public static String starStaircaseReverse(int height){
-    return "";
+    String answer = "";
+    for (int i = height; i >= 1; i--) {
+        for (int k = 1; k <= i; k++) {
+            answer = answer + "*";
+        }
+        if (i != 1) {
+            answer = answer + "\n";
+        }
+    }
+    return answer;
 }
             /*
 
@@ -41,7 +59,28 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
-        return "";
+        String answer = "";
+        if (width == 1 || height == 1) {
+            return answer;
+        }
+        for (int i = 1; i <= height; i++) {
+            if (i == 1 || i == height) {
+                for (int k = 1; k <= width; k++) {
+                    answer = answer + "*";
+                }
+            } else {
+                for (int k = 1; k <= width; k++) {
+                    if (k == 1 || k == width) {
+                        answer = answer + "*";
+                    } else {
+                        answer = answer + " ";
+                    }
+                }
+            }
+            
+            answer = answer + "\n";
+        }
+        return answer.trim();
     }
                     /*
 return the string below for word="HELLO" rows=5                    
@@ -54,7 +93,23 @@ OHELL
   i will provide a hint for this one if you get stuck.. use modulo
                 */
     public static String repeatRectangle(String word, int rows){
-        return "";
+        String answer = "";
+        String word1 = word;
+        for (int i = 0; i < rows; i++) {
+            int k = i;
+            if (i >= word.length()) {
+                k = i % word.length();
+            }
+            int length = word.length();
+            if (i == 0) {
+                answer = answer + word;
+            } else {
+                word1 = word.substring(k) + word.substring(0,k);
+                answer = answer + word1;
+            }
+            answer = answer + "\n";
+        }
+        return answer.trim();
     }
 
 }
--- src/main/java/com/example/project/NestedLoopsRunner.java
+++ src/main/java/com/example/project/NestedLoopsRunner.java
@@ -2,7 +2,11 @@ package com.example.project;
 
 public class NestedLoopsRunner {
     public static void main(String[] args) {
-     
+     NestedLoops speed = new NestedLoops(); 
+     System.out.println(speed.starStaircase(5));
+     System.out.println(speed.starStaircaseReverse(5));
+    System.out.println(speed.emptyBox(5,5));
+    System.out.println(speed.repeatRectangle("AB", 10));
     }
 }
 
